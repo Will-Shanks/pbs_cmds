16abc543a9433bbbcded6ae243dea2e04fa9c92d
Will Shanks <shanks@ucar.edu>
2023-12-05 13:06:54 -0700

    add github actions

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use clap::{Parser, Subcommand};
-use pbs::{Attribs, Server, Status, StatResp, ResvSubFlag, ResvModFlag};
-
+use pbs::{Attribs, ResvModFlag, ResvSubFlag, Server, StatResp, Status};
 
 #[derive(Debug, Parser)]
 #[command(name = "wpbs")]
@@ -38,74 +37,102 @@ enum Noun {
     Job(JobVerb),
     #[command(subcommand)]
     Host(StatVerb),
-    #[command(subcommand, name="resv")]
+    #[command(subcommand, name = "resv")]
     Reservation(ResvVerb),
     #[command(subcommand)]
     Resource(StatVerb),
-    #[command(name="vnode", subcommand)]
+    #[command(name = "vnode", subcommand)]
     Vnode(StatVerb),
     #[command(subcommand)]
     Que(StatVerb),
-    #[command(subcommand, name="sched")]
+    #[command(subcommand, name = "sched")]
     Scheduler(StatVerb),
-    #[command(subcommand, name="srv")]
+    #[command(subcommand, name = "srv")]
     Server(StatVerb),
 }
 
-#[derive(Debug,Default, clap::Args)]
+#[derive(Debug, Default, clap::Args)]
 pub struct ModAttribs {
-    #[arg(help="name")]
+    #[arg(help = "name")]
     name: String,
-    #[arg(help="attributes")]
+    #[arg(help = "attributes")]
     attribs: Vec<String>,
-    #[arg(short, long, action, help="-Wforce option")]
+    #[arg(short, long, action, help = "-Wforce option")]
     force: bool,
-
 }
 
-#[derive(Debug,Default, clap::Args)]
+#[derive(Debug, Default, clap::Args)]
 pub struct DelAttribs {
-    #[arg(help="name")]
+    #[arg(help = "name")]
     name: String,
 }
 
-#[derive(Debug,Default, clap::Args)]
+#[derive(Debug, Default, clap::Args)]
 pub struct ResvSub {
-    #[arg(help="attributes")]
+    #[arg(help = "attributes")]
     attribs: Vec<String>,
-    #[arg(short, long, action, help="Create maintenance reservation")]
+    #[arg(short, long, action, help = "Create maintenance reservation")]
     maintenance: bool,
 }
 
-#[derive(Debug,Default, clap::Args)]
+#[derive(Debug, Default, clap::Args)]
 pub struct Submit {
-    #[arg(help="attributes")]
+    #[arg(help = "attributes")]
     attribs: Vec<String>,
-    #[arg(short, long, help="job script")]
+    #[arg(short, long, help = "job script")]
     script: String,
-    #[arg(short, long, help="job que")]
+    #[arg(short, long, help = "job que")]
     que: String,
-    #[arg(short, long, action, help="don't print any attributes, only names")]
+    #[arg(short, long, action, help = "don't print any attributes, only names")]
     no_attribs: bool,
-    #[arg(short, long, action, conflicts_with="no_attribs", conflicts_with="json", help="print resources over multiple lines")]
+    #[arg(
+        short,
+        long,
+        action,
+        conflicts_with = "no_attribs",
+        conflicts_with = "json",
+        help = "print resources over multiple lines"
+    )]
     long: bool,
-    #[arg(short, long, action, conflicts_with="long", help="output in json")]
+    #[arg(short, long, action, conflicts_with = "long", help = "output in json")]
     json: bool,
 }
 
-#[derive(Debug,Default, clap::Args)]
+#[derive(Debug, Default, clap::Args)]
 pub struct Filter {
-    #[arg(help="name, or nameset ex: casper1[2-7]")]
+    #[arg(help = "name, or nameset ex: casper1[2-7]")]
     name: Option<String>,
-    #[arg(short, long, help="filter attributes, ex: state=free, can use =, !=, <, <=, >, and >=")]
+    #[arg(
+        short,
+        long,
+        help = "filter attributes, ex: state=free, can use =, !=, <, <=, >, and >="
+    )]
     attribs: Vec<String>,
-    #[arg(short, long, conflicts_with="no_attribs", help="attributes to display, ex: state")]
+    #[arg(
+        short,
+        long,
+        conflicts_with = "no_attribs",
+        help = "attributes to display, ex: state"
+    )]
     out: Option<Vec<String>>,
-    #[arg(short, long, action, conflicts_with="out", help="don't print any attributes, only names")]
+    #[arg(
+        short,
+        long,
+        action,
+        conflicts_with = "out",
+        help = "don't print any attributes, only names"
+    )]
     no_attribs: bool,
-    #[arg(short, long, action, conflicts_with="no_attribs", conflicts_with="json", help="print resources over multiple lines")]
+    #[arg(
+        short,
+        long,
+        action,
+        conflicts_with = "no_attribs",
+        conflicts_with = "json",
+        help = "print resources over multiple lines"
+    )]
     long: bool,
-    #[arg(short, long, action, conflicts_with="long", help="output in json")]
+    #[arg(short, long, action, conflicts_with = "long", help = "output in json")]
     json: bool,
 }
 
@@ -141,12 +168,12 @@ impl Filter {
     fn names(&self) -> Vec<String> {
         if let Some(name) = &self.name {
             hostlist_parser::parse(name).unwrap()
-        }else{
-            vec!()
+        } else {
+            vec![]
         }
     }
     fn check(&self, status: &Status) -> bool {
-        if self.name.is_some(){
+        if self.name.is_some() {
             let mut found = false;
             for n in self.names() {
                 if status.name().starts_with(&n) {
@@ -154,14 +181,22 @@ impl Filter {
                     break;
                 }
             }
-            if !found { return false }
+            if !found {
+                return false;
+            }
         }
-        if !status.attribs().check_filter(&self.attribs()) { return false }
+        if !status.attribs().check_filter(&self.attribs()) {
+            return false;
+        }
         true
     }
     fn printfmt(&self) -> Printfmt {
-        if self.json { return Printfmt::Json; }
-        if self.long { return Printfmt::Long }
+        if self.json {
+            return Printfmt::Json;
+        }
+        if self.long {
+            return Printfmt::Long;
+        }
         Printfmt::Long
         //Printfmt::Short
     }
@@ -171,22 +206,27 @@ fn handle_stat(data: &StatResp, attribs: &Filter) {
     let data = data.resources.iter().filter(|r| attribs.check(r));
     match attribs.printfmt() {
         Printfmt::Long => {
-            data.for_each( |item| {
-                println!("{}",item.name());
+            data.for_each(|item| {
+                println!("{}", item.name());
                 if !attribs.no_attribs {
-                    println!("{}",item.attribs());
+                    println!("{}", item.attribs());
                 }
             });
-        },
+        }
         //Printfmt::Short => todo!(),
-        Printfmt::Json  => {
-            let out: Vec<_> = data.map(|item|{
-                let mut attribs = item.attribs().json();
-                attribs.as_object_mut().unwrap().insert("name".to_string(), serde_json::Value::String(item.name()));
-                attribs
-            }).collect();
+        Printfmt::Json => {
+            let out: Vec<_> = data
+                .map(|item| {
+                    let mut attribs = item.attribs().json();
+                    attribs
+                        .as_object_mut()
+                        .unwrap()
+                        .insert("name".to_string(), serde_json::Value::String(item.name()));
+                    attribs
+                })
+                .collect();
             println!("{}", serde_json::to_string(&out).unwrap());
-        },
+        }
     }
 }
 
@@ -199,22 +239,23 @@ fn main() {
         Server::new()
     };
     match args.noun {
-        Noun::Job(verb) => {
-            match verb {
-                JobVerb::Stat(attribs) => {
-                    handle_stat(&srv.stat_job(attribs.attribs(), attribs.out()).unwrap(), &attribs);
-                },
-                JobVerb::Sub(attribs) => {
-                    let resp = &srv.submit_job(attribs.attribs(), &attribs.script, &attribs.que);
-                    println!("{resp:?}");
-                },
-                JobVerb::Del(attribs)  => {
-                    if let Err(e) = &srv.del_job(&attribs.name) {
-                        println!("Error deleting job: {e}");
-                    } else {
-                        println!("Job deleted");
-                    }
-                },
+        Noun::Job(verb) => match verb {
+            JobVerb::Stat(attribs) => {
+                handle_stat(
+                    &srv.stat_job(attribs.attribs(), attribs.out()).unwrap(),
+                    &attribs,
+                );
+            }
+            JobVerb::Sub(attribs) => {
+                let resp = &srv.submit_job(attribs.attribs(), &attribs.script, &attribs.que);
+                println!("{resp:?}");
+            }
+            JobVerb::Del(attribs) => {
+                if let Err(e) = &srv.del_job(&attribs.name) {
+                    println!("Error deleting job: {e}");
+                } else {
+                    println!("Job deleted");
+                }
             }
         },
         Noun::Host(verb) => {
@@ -223,81 +264,88 @@ fn main() {
                     //TODO don't do a full host stat if attribs.names().len() is short
                     let resp = srv.stat_host(&None, attribs.out());
                     handle_stat(&resp.unwrap(), &attribs);
-                },
+                }
             }
-        },
+        }
         Noun::Reservation(verb) => {
             match verb {
                 ResvVerb::Stat(attribs) => {
                     //TODO don't do a full stat if attribs.names().len() is short
-                    handle_stat(&srv.stat_reservation(&None, attribs.out()).unwrap(), &attribs);
-                },
+                    handle_stat(
+                        &srv.stat_reservation(&None, attribs.out()).unwrap(),
+                        &attribs,
+                    );
+                }
                 ResvVerb::Sub(attribs) => {
                     let mut flags = Vec::new();
-                    if attribs.maintenance {flags.push(ResvSubFlag::Maintenance)};
-                    let resp =srv.submit_resv(attribs.attribs(), flags);
-                    match &resp{
+                    if attribs.maintenance {
+                        flags.push(ResvSubFlag::Maintenance)
+                    };
+                    let resp = srv.submit_resv(attribs.attribs(), flags);
+                    match &resp {
                         Err(e) => println!("Error submitting reservation: {e}"),
                         Ok(id) => println!("Reservation submitted id: {id}"),
                     }
-                },
+                }
                 ResvVerb::Mod(attribs) => {
                     let mut flags = Vec::new();
-                    if attribs.force {flags.push(ResvModFlag::Force)};
+                    if attribs.force {
+                        flags.push(ResvModFlag::Force)
+                    };
                     let resp = srv.mod_resv(&attribs.name, attribs.attribs(), flags);
                     match &resp {
                         Err(e) => println!("Error submitting reservation: {e}"),
                         Ok(id) => println!("Reservation submitted id: {id}"),
                     }
-                },
+                }
                 ResvVerb::Del(attribs) => {
                     if let Err(e) = &srv.del_resv(&attribs.name) {
                         println!("Error deleting reservation: {e}");
                     } else {
                         println!("Reservation deleted");
                     }
-                },
+                }
             }
-        },
+        }
         Noun::Resource(verb) => {
             match verb {
                 StatVerb::Stat(attribs) => {
                     //TODO don't do a full if attribs.names().len() is short
                     handle_stat(&srv.stat_resource(&None, attribs.out()).unwrap(), &attribs);
-                },
+                }
             }
-        },
+        }
         Noun::Vnode(verb) => {
             match verb {
                 StatVerb::Stat(attribs) => {
                     //TODO don't do a full stat if attribs.names().len() is short
                     handle_stat(&srv.stat_vnode(&None, attribs.out()).unwrap(), &attribs);
-                },
+                }
             }
-        },
+        }
         Noun::Que(verb) => {
             match verb {
                 StatVerb::Stat(attribs) => {
                     //TODO don't do a full stat if attribs.names().len() is short
                     handle_stat(&srv.stat_que(&None, attribs.out()).unwrap(), &attribs);
-                },
+                }
             }
-        },
+        }
         Noun::Scheduler(verb) => {
             match verb {
                 StatVerb::Stat(attribs) => {
                     //TODO don't do a full stat if attribs.names().len() is short
                     handle_stat(&srv.stat_scheduler(&None, attribs.out()).unwrap(), &attribs);
-                },
+                }
             }
-        },
+        }
         Noun::Server(verb) => {
             match verb {
                 StatVerb::Stat(attribs) => {
                     //TODO don't do a full stat if attribs.names().len() is short
                     handle_stat(&srv.stat_scheduler(&None, attribs.out()).unwrap(), &attribs);
-                },
+                }
             }
-        },
+        }
     }
 }
